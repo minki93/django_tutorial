b57410e370c9b1edf20ff0ea59e137ef56672598
minki93 <minki@DESKTOP-UEQK925.localdomain>
2022-06-10 18:20:19 +0900

    수정

--- pybo/models.py
+++ pybo/models.py
@@ -11,3 +11,17 @@ class Question(models.Model):
     subject = models.CharField(max_length=200) # 질문의 제목
     content = models.TextField() # 질문의 내용
     create_date = models.DateTimeField() # 질문을 작성한 일시
+
+    def __str__(self):
+        """
+        조회 결과에 속성값으로 보여주기
+        """
+        return self.subject
+
+class Answer(models.Model):
+    """
+    답변 모델 작성
+    """
+    question = models.ForeignKey(Question, on_delete = models.CASCADE) # 질문 -- 어떤 질문의 답변인지 알아야하므로, 질문 속성 필요
+    content = models.TextField() # 답변 내용
+    create_date = models.DateTimeField() # 답변 작성 일시
